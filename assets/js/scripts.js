bbf2e1f2e72507cf49d0a9d805e8c34d30edda03
Atrion Darnay <Atrion@gmail.com>
2025-08-11 20:26:13 -0300

    Revert "Changed Menu to Hamburger"
    This reverts commit eb72932b35df957c678d778440b3eb1f7a050d25.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,18 +5,8 @@ $( document ).ready(function() {
 	var dropcaps = document.querySelectorAll(".dropcap");
 	window.Dropcap.layout(dropcaps, 2);
 
-// Create the mobile toggle without a text label
-var nav = responsiveNav(".nav-collapse", { label: "" });
-
-// Replace the auto-created toggle's content with the Bootstrap "list" icon
-var toggle = document.querySelector(".nav-toggle");
-if (toggle) {
-  toggle.setAttribute("aria-label", "Open navigation");
-  toggle.innerHTML =
-    '<svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true" focusable="false">' +
-      '<path fill-rule="evenodd" d="M2.5 12.5a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-11zm0-4a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-11zm0-4a.5.5 0 0 1 0-1h11a.5.5 0 0 1 0 1h-11z"/>' +
-    "</svg>";
-}
+	// Responsive-Nav
+	var nav = responsiveNav(".nav-collapse");
 
 	// Round Reading Time
     $(".time").text(function (index, value) {
